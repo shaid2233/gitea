9eeb37e50746e87086325bfcace33b3c126eabfd
Shai Daniel <155896805+shaid2233@users.noreply.github.com>
2025-02-09 16:45:26 +0200

    Update main.py

--- mysql_alive/main.py
+++ mysql_alive/main.py
@@ -5,11 +5,11 @@ import os
 app = Flask(__name__)
 
 # Environment variables for MySQL connection
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_USER = os.getenv("DB_USER", "root")  # Optional, default user
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")  # Optional, default password
-DB_NAME = os.getenv("DB_NAME", "testdb")  # Optional, default database
+DB_HOST = os.getenv("DB_HOST", "#####")
+DB_PORT = os.getenv("DB_PORT", "#####")
+DB_USER = os.getenv("DB_USER", "#####")  # Optional, default user
+DB_PASSWORD = os.getenv("DB_PASSWORD", "#####")  # Optional, default password
+DB_NAME = os.getenv("DB_NAME", "#####")  # Optional, default database
 
 
 @app.route("/is-db-alive", methods=["GET"])
